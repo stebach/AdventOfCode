2e66d8109f646f99c6eba132270e69fa7914036e
ste <ste@ste.li>
2022-12-03 09:49:54 +0100

    Day 3

--- src/main/java/li/ste/adventofcode2022/days/Day03.java
+++ src/main/java/li/ste/adventofcode2022/days/Day03.java
@@ -1,7 +1,10 @@
 package li.ste.adventofcode2022.days;
 
+import li.ste.adventofcode2022.utils.AdventOfCodeException;
 import li.ste.adventofcode2022.utils.Day;
 import li.ste.adventofcode2022.utils.InputProvider;
+
+import java.util.List;
 
 public class Day03 extends Day {
     public Day03(InputProvider provider) {
@@ -10,7 +13,52 @@ public class Day03 extends Day {
 
     @Override
     public void run() {
-        setSolution1("@todo");
-        setSolution2("@todo");
+        List<String> data = getData();
+
+        int solution1 = 0;
+        int solution2 = 0;
+
+        for (int i=0; i<data.size(); i++) {
+            String rucksackContents = data.get(i);
+            String compartment1 = rucksackContents.substring(0, rucksackContents.length() / 2);
+            String compartment2 = rucksackContents.substring(rucksackContents.length() / 2);
+
+            char item = searchCommonItem(compartment1, new String[] { compartment2 });
+            solution1 += priorityForItem(item);
+
+            if (i % 3 == 0) {
+                char badgeItm = searchCommonItem(rucksackContents, new String[] {
+                        data.get(i + 1),
+                        data.get(i + 2)
+                });
+                solution2 += priorityForItem(badgeItm);
+            }
+        }
+        setSolution1(solution1);
+        setSolution2(solution2);
+    }
+
+    private char searchCommonItem(String first, String[] others) {
+        for (char item : first.toCharArray()) {
+            boolean wrongItem = false;
+            for (String other : others) {
+                if (!other.contains(item+"")) {
+                    wrongItem = true;
+                    break;
+                }
+            }
+            if (!wrongItem) {
+                return item;
+            }
+        }
+        throw new AdventOfCodeException("no matching item found");
+    }
+
+    private int priorityForItem(char item) {
+        if (item < 'a') {
+            return item - 'A' + 27;
+        } else {
+            return item - 'a' + 1;
+        }
     }
 }
--- src/test/java/li/ste/adventofcode2022/days/TestDay03.java
+++ src/test/java/li/ste/adventofcode2022/days/TestDay03.java
@@ -15,11 +15,11 @@ public class TestDay03 {
 
     @Test
     void testSolution1() {
-        assertEquals("@todo", day.getSolution1()); // TODO: check solution 1 of day 3
+        assertEquals("157", day.getSolution1());
     }
 
     @Test
     void testSolution2() {
-        assertEquals("@todo", day.getSolution2()); // TODO: check solution 2 of day 3
+        assertEquals("70", day.getSolution2());
     }
 }
