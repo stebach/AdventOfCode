93279fd64891de1f3b42b31c679eea3eed135266
ste <ste@ste.li>
2022-12-04 07:41:56 +0100

    Day 4

--- src/main/java/li/ste/adventofcode/year2022/Day04.java
+++ src/main/java/li/ste/adventofcode/year2022/Day04.java
@@ -2,6 +2,9 @@ package li.ste.adventofcode.year2022;
 
 import li.ste.adventofcode.utils.Day;
 import li.ste.adventofcode.utils.InputProvider;
+import li.ste.adventofcode.year2022.day04.SectionRangePair;
+
+import java.util.List;
 
 public class Day04 extends Day {
     public static void main(String[] args) {
@@ -15,7 +18,20 @@ public class Day04 extends Day {
 
     @Override
     public void run() {
-        setSolution1("@todo");
-        setSolution2("@todo");
+        List<SectionRangePair> data = getRegexData("^([0-9]+)-([0-9]+),([0-9]+)-([0-9]+)$", SectionRangePair.class);
+
+        int solution1 = 0;
+        int solution2 = 0;
+        for (SectionRangePair pair : data) {
+            if (pair.firstContainsSecond() || pair.secondContainsFirst()) {
+                solution1++;
+            }
+            if (pair.overlaps()) {
+                solution2++;
+            }
+        }
+        setSolution1(solution1);
+        setSolution2(solution2);
     }
+
 }
---
+++ src/main/java/li/ste/adventofcode/year2022/day04/SectionRangePair.java
@@ -0,0 +1,31 @@
+package li.ste.adventofcode.year2022.day04;
+
+import li.ste.adventofcode.utils.RegexResultRecipient;
+
+public class SectionRangePair implements RegexResultRecipient {
+    private int from1;
+    private int to1;
+    private int from2;
+    private int to2;
+
+    @Override
+    public void setRegexResult(String[] listEntry) {
+        from1 = Integer.parseInt(listEntry[0]);
+        to1 = Integer.parseInt(listEntry[1]);
+        from2 = Integer.parseInt(listEntry[2]);
+        to2 = Integer.parseInt(listEntry[3]);
+    }
+
+    public boolean firstContainsSecond() {
+        return from1 <= from2 && to1 >= to2;
+    }
+
+    public boolean secondContainsFirst() {
+        return from2 <= from1 && to2 >= to1;
+    }
+
+    public boolean overlaps() {
+        return ((from1 <= from2 && to1 >= from2) || (from2 <= from1 && to2 >= from1));
+    }
+}
+
