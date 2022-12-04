30e15e289cb23cc7eef5b22144bb804df6a65535
ste <ste@ste.li>
2022-12-04 18:05:41 +0100

    2020 Day 1

--- src/main/java/li/ste/adventofcode/year2020/Day01.java
+++ src/main/java/li/ste/adventofcode/year2020/Day01.java
@@ -3,6 +3,8 @@ package li.ste.adventofcode.year2020;
 import li.ste.adventofcode.utils.Day;
 import li.ste.adventofcode.utils.InputProvider;
 
+import java.util.List;
+
 public class Day01 extends Day {
     public static void main(String[] args) {
         Day day = new Day01(new InputProvider());
@@ -15,7 +17,20 @@ public class Day01 extends Day {
 
     @Override
     public void run() {
-        setSolution1("@todo");
-        setSolution2("@todo");
+        List<Integer> data = getIntData();
+        for (Integer number : data) {
+            if (data.contains(2020-number)) {
+                setSolution1(number * (2020-number));
+                break;
+            }
+        }
+        for (int i=0; i<data.size()-2; i++) {
+            for (int j=i+1; j<data.size()-1; j++) {
+                if (data.contains(2020-data.get(i)-data.get(j))) {
+                    setSolution2(data.get(i) * data.get(j) * (2020-data.get(i)-data.get(j)));
+                    break;
+                }
+            }
+        }
     }
 }
