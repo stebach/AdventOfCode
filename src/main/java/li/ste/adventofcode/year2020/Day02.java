b8fc353257c2ec0563659c293e109579cb422ef7
ste <ste@ste.li>
2022-12-04 18:21:35 +0100

    2020 Day 2

--- src/main/java/li/ste/adventofcode/year2020/Day02.java
+++ src/main/java/li/ste/adventofcode/year2020/Day02.java
@@ -2,6 +2,9 @@ package li.ste.adventofcode.year2020;
 
 import li.ste.adventofcode.utils.Day;
 import li.ste.adventofcode.utils.InputProvider;
+import li.ste.adventofcode.year2020.day02.PasswordEntry;
+
+import java.util.List;
 
 public class Day02 extends Day {
     public static void main(String[] args) {
@@ -15,7 +18,18 @@ public class Day02 extends Day {
 
     @Override
     public void run() {
-        setSolution1("@todo");
-        setSolution2("@todo");
+        List<PasswordEntry> data = getRegexData("^([0-9]*)-([0-9]*) ([a-z]): ([a-z]*)$", PasswordEntry.class);
+        int solution1 = 0;
+        int solution2 = 0;
+        for (PasswordEntry entry : data) {
+            if (entry.isValid()) {
+                solution1++;
+            }
+            if (entry.isValid2()) {
+                solution2++;
+            }
+        }
+        setSolution1(solution1);
+        setSolution2(solution2);
     }
 }
--- src/test/java/li/ste/adventofcode/year2020/TestDay02.java
+++ src/test/java/li/ste/adventofcode/year2020/TestDay02.java
@@ -20,11 +20,11 @@ public class TestDay02 {
 
     @Test
     void testSolution1() {
-        assertEquals("@todo", day.getSolution1()); // TODO: check solution 1 of day 02
+        assertEquals("2", day.getSolution1());
     }
 
     @Test
     void testSolution2() {
-        assertEquals("@todo", day.getSolution2()); // TODO: check solution 2 of day 02
+        assertEquals("1", day.getSolution2());
     }
 }
