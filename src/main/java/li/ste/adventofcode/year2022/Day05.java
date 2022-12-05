165b4574ba56bf32accce1ab83d2e8d0e02e12f3
ste <ste@ste.li>
2022-12-05 07:43:40 +0100

    Day 5

--- src/main/java/li/ste/adventofcode/year2022/Day05.java
+++ src/main/java/li/ste/adventofcode/year2022/Day05.java
@@ -3,7 +3,14 @@ package li.ste.adventofcode.year2022;
 import li.ste.adventofcode.utils.Day;
 import li.ste.adventofcode.utils.InputProvider;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Day05 extends Day {
+    private final List<List<Character>> stacks = new ArrayList<>();
+    private final List<List<Character>> stacks2 = new ArrayList<>();
+
     public static void main(String[] args) {
         Day day = new Day05(new InputProvider());
         day.solvePuzzles();
@@ -15,7 +22,59 @@ public class Day05 extends Day {
 
     @Override
     public void run() {
-        setSolution1("@todo");
-        setSolution2("@todo");
+        List<String> data = getData();
+        int line;
+        for (line=0; line<data.size(); line++) {
+            if (!parseInitialPositionLine(data, line)) {
+                break;
+            }
+        }
+
+        for (int instructionLine=line+2; instructionLine<data.size(); instructionLine++) {
+            String[] instructionParts = data.get(instructionLine).split(" ");
+            moveCrates(stacks, Integer.parseInt(instructionParts[3]), Integer.parseInt(instructionParts[5]), Integer.parseInt(instructionParts[1]), false);
+            moveCrates(stacks2, Integer.parseInt(instructionParts[3]), Integer.parseInt(instructionParts[5]), Integer.parseInt(instructionParts[1]), true);
+        }
+
+        setSolution1(getTopCrates(stacks));
+        setSolution2(getTopCrates(stacks2));
+    }
+
+    private boolean parseInitialPositionLine(List<String> data, int line) {
+        boolean startingStackFound = false;
+        for (int charPos=0; charPos<data.get(line).length(); charPos+=4) {
+            if (data.get(line).charAt(charPos) == '[') {
+                startingStackFound = true;
+                while (stacks.size() < charPos/4+1) {
+                    stacks.add(new ArrayList<>());
+                }
+                while (stacks2.size() < charPos/4+1) {
+                    stacks2.add(new ArrayList<>());
+                }
+                stacks.get(charPos/4).add(0, data.get(line).charAt(charPos+1));
+                stacks2.get(charPos/4).add(0, data.get(line).charAt(charPos+1));
+            }
+        }
+        return startingStackFound;
+    }
+
+    public String getTopCrates(List<List<Character>> stacks) {
+        StringBuilder topCrates = new StringBuilder();
+        for (List<Character> stack : stacks) {
+            if (!stack.isEmpty()) {
+                topCrates.append(stack.get(stack.size()-1));
+            }
+        }
+        return topCrates.toString();
+    }
+
+    public void moveCrates(List<List<Character>> stacks, int from, int to, int amount, boolean multipleAtOnce) {
+        List<Character> fromStack = stacks.get(from - 1);
+        List<Character> toMove = fromStack.subList(fromStack.size() - amount, fromStack.size());
+        stacks.set(from-1, fromStack.subList(0, fromStack.size() - amount));
+        if (!multipleAtOnce) {
+            Collections.reverse(toMove);
+        }
+        stacks.get(to-1).addAll(toMove);
     }
 }
--- src/test/java/li/ste/adventofcode/year2022/TestDay05.java
+++ src/test/java/li/ste/adventofcode/year2022/TestDay05.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayNameGeneration(li.ste.adventofcode.DisplayNameGenerator.class)
@@ -19,12 +22,70 @@ public class TestDay05 {
     }
 
     @Test
+    void testMove() {
+        List<List<Character>> stacks = new ArrayList<>();
+        stacks.add(new ArrayList<>());
+        stacks.get(0).add('Z');
+        stacks.get(0).add('N');
+        stacks.add(new ArrayList<>());
+        stacks.get(1).add('M');
+        stacks.get(1).add('C');
+        stacks.get(1).add('D');
+        stacks.add(new ArrayList<>());
+        stacks.get(2).add('P');
+
+        day.moveCrates(stacks, 2, 1, 1, false);
+        assertEquals(3, stacks.get(0).size());
+        assertEquals(2, stacks.get(1).size());
+        assertEquals("DCP", day.getTopCrates(stacks));
+
+        day.moveCrates(stacks, 1,3,3,false);
+        assertEquals(0, stacks.get(0).size());
+        assertEquals(4, stacks.get(2).size());
+        assertEquals("CZ", day.getTopCrates(stacks));
+
+        day.moveCrates(stacks, 2,1,2,false);
+        assertEquals(2, stacks.get(0).size());
+        assertEquals(0, stacks.get(1).size());
+        assertEquals("MZ", day.getTopCrates(stacks));
+    }
+
+    @Test
+    void testMoveMultiple() {
+        List<List<Character>> stacks = new ArrayList<>();
+        stacks.add(new ArrayList<>());
+        stacks.get(0).add('Z');
+        stacks.get(0).add('N');
+        stacks.add(new ArrayList<>());
+        stacks.get(1).add('M');
+        stacks.get(1).add('C');
+        stacks.get(1).add('D');
+        stacks.add(new ArrayList<>());
+        stacks.get(2).add('P');
+
+        day.moveCrates(stacks, 2, 1, 1, true);
+        assertEquals(3, stacks.get(0).size());
+        assertEquals(2, stacks.get(1).size());
+        assertEquals("DCP", day.getTopCrates(stacks));
+
+        day.moveCrates(stacks, 1,3,3,true);
+        assertEquals(0, stacks.get(0).size());
+        assertEquals(4, stacks.get(2).size());
+        assertEquals("CD", day.getTopCrates(stacks));
+
+        day.moveCrates(stacks, 2,1,2,true);
+        assertEquals(2, stacks.get(0).size());
+        assertEquals(0, stacks.get(1).size());
+        assertEquals("CD", day.getTopCrates(stacks));
+    }
+
+    @Test
     void testSolution1() {
-        assertEquals("@todo", day.getSolution1()); // TODO: check solution 1 of day 05
+        assertEquals("CMZ", day.getSolution1());
     }
 
     @Test
     void testSolution2() {
-        assertEquals("@todo", day.getSolution2()); // TODO: check solution 2 of day 05
+        assertEquals("MCD", day.getSolution2());
     }
 }
