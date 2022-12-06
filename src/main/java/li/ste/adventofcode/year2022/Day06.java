341234620f3f0016373e38003a16738ddc4e4011
ste <ste@ste.li>
2022-12-06 06:23:42 +0100

    Day 6

--- src/main/java/li/ste/adventofcode/year2022/Day06.java
+++ src/main/java/li/ste/adventofcode/year2022/Day06.java
@@ -3,6 +3,10 @@ package li.ste.adventofcode.year2022;
 import li.ste.adventofcode.utils.Day;
 import li.ste.adventofcode.utils.InputProvider;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Day06 extends Day {
     public static void main(String[] args) {
         Day day = new Day06(new InputProvider());
@@ -15,7 +19,25 @@ public class Day06 extends Day {
 
     @Override
     public void run() {
-        setSolution1("@todo");
-        setSolution2("@todo");
+        char[] data = getData().get(0).toCharArray();
+        setSolution1(getStartOfFirstAllUniqueString(data, 4)+4);
+        setSolution2(getStartOfFirstAllUniqueString(data, 14)+14);
+    }
+
+    private int getStartOfFirstAllUniqueString(char[] data, int length) {
+        for (int i=0; i<data.length-(length-1); i++) {
+            if (allCharactersUnique(Arrays.copyOfRange(data,i,i+length))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private boolean allCharactersUnique(char[] characters) {
+        Set<Character> check = new HashSet<>();
+        for (char character : characters) {
+            check.add(character);
+        }
+        return check.size() == characters.length;
     }
 }
--- src/test/java/li/ste/adventofcode/year2022/TestDay06.java
+++ src/test/java/li/ste/adventofcode/year2022/TestDay06.java
@@ -20,11 +20,11 @@ public class TestDay06 {
 
     @Test
     void testSolution1() {
-        assertEquals("@todo", day.getSolution1()); // TODO: check solution 1 of day 06
+        assertEquals("7", day.getSolution1());
     }
 
     @Test
     void testSolution2() {
-        assertEquals("@todo", day.getSolution2()); // TODO: check solution 2 of day 06
+        assertEquals("19", day.getSolution2());
     }
 }
