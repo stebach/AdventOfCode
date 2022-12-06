a4f4e2c2b885297d458c14d7ca5fd687f7b37700
ste <ste@ste.li>
2022-12-06 07:57:58 +0100

    2020 Day 6

--- src/main/java/li/ste/adventofcode/year2020/Day06.java
+++ src/main/java/li/ste/adventofcode/year2020/Day06.java
@@ -3,6 +3,9 @@ package li.ste.adventofcode.year2020;
 import li.ste.adventofcode.utils.Day;
 import li.ste.adventofcode.utils.InputProvider;
 
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Day06 extends Day {
     public static void main(String[] args) {
         Day day = new Day06(new InputProvider());
@@ -15,7 +18,30 @@ public class Day06 extends Day {
 
     @Override
     public void run() {
-        setSolution1("@todo");
-        setSolution2("@todo");
+        List<String> data = getData();
+        data.add("");
+        Map<Character, Integer> groupAnswers = new HashMap<>();
+
+        int solution1 = 0;
+        int solution2 = 0;
+        AtomicInteger personCount = new AtomicInteger();
+        for (String row : data) {
+            if (row.length() > 0) {
+                personCount.incrementAndGet();
+                char[] groupChars = row.toCharArray();
+                for (char groupChar : groupChars) {
+                    groupAnswers.putIfAbsent(groupChar, 0);
+                    groupAnswers.compute(groupChar, (k,v) -> v += 1);
+                }
+            } else {
+                solution2 += groupAnswers.entrySet().stream().filter(entry -> entry.getValue() == personCount.intValue()).count();
+                solution1 += groupAnswers.size();
+                groupAnswers.clear();
+                personCount.set(0);
+            }
+        }
+
+        setSolution1(solution1);
+        setSolution2(solution2);
     }
 }
