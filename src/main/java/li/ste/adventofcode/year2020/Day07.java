67b6133047b44bb435b7c9779bf39eb673886a97
ste <ste@ste.li>
2022-12-07 13:05:41 +0100

    2020 Day 7

--- src/main/java/li/ste/adventofcode/year2020/Day07.java
+++ src/main/java/li/ste/adventofcode/year2020/Day07.java
@@ -2,6 +2,9 @@ package li.ste.adventofcode.year2020;
 
 import li.ste.adventofcode.utils.Day;
 import li.ste.adventofcode.utils.InputProvider;
+import li.ste.adventofcode.year2020.day07.LuggageRule;
+
+import java.util.List;
 
 public class Day07 extends Day {
     public static void main(String[] args) {
@@ -15,7 +18,15 @@ public class Day07 extends Day {
 
     @Override
     public void run() {
-        setSolution1("@todo");
-        setSolution2("@todo");
+        List<LuggageRule> data = getRegexData("^(.*?) bags contain (.*)\\.$", LuggageRule.class);
+        String checkFor = "shiny gold";
+        int solution1 = 0;
+        for (LuggageRule luggageRule : data) {
+            if (luggageRule.canHold(checkFor, data)) {
+                solution1++;
+            }
+        }
+        setSolution1(solution1);
+        setSolution2(data.stream().filter(r -> r.getColor().equals(checkFor)).findFirst().get().getContentCount(data));
     }
 }
