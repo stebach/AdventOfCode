51fe6f6eb3c03fe7c4f0879e2e3000781d622bd1
ste <ste@ste.li>
2022-12-07 15:48:49 +0100

    Day 7 optimization

--- src/main/java/li/ste/adventofcode/year2022/Day07.java
+++ src/main/java/li/ste/adventofcode/year2022/Day07.java
@@ -2,10 +2,9 @@ package li.ste.adventofcode.year2022;
 
 import li.ste.adventofcode.utils.Day;
 import li.ste.adventofcode.utils.InputProvider;
-import li.ste.adventofcode.year2022.day07.FileSystemFolder;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Day07 extends Day {
     public static void main(String[] args) {
@@ -19,37 +18,42 @@ public class Day07 extends Day {
 
     @Override
     public void run() {
-        FileSystemFolder fs = parseData();
+        Map<String, Long> folderSizes = parseData();
 
-        List<FileSystemFolder> folderWithMaxSize100000 = fs.getFoldersWithMaxSize(100000);
+
         long solution1 = 0;
-        for (FileSystemFolder fileSystemFolder : folderWithMaxSize100000) {
-            solution1 += fileSystemFolder.getSize();
+        long solution2 = Long.MAX_VALUE;
+        long minSizeForSolution2 = 30000000 - (70000000 - folderSizes.get("/"));
+        for (Long value : folderSizes.values()) {
+            if (value <= 100000) {
+                solution1 += value;
+            }
+            if (value < solution2 && value >= minSizeForSolution2) {
+                solution2 = value;
+            }
         }
         setSolution1(solution1);
-
-        List<FileSystemFolder> potentialFoldersToDelete = fs.getFoldersWithMinSize(
-                30000000 - (70000000 - fs.getSize()));
-        potentialFoldersToDelete.sort(Comparator.comparingLong(FileSystemFolder::getSize));
-
-        setSolution2(potentialFoldersToDelete.get(0).getSize());
+        setSolution2(solution2);
     }
 
-    private FileSystemFolder parseData() {
+    private Map<String, Long> parseData() {
         List<String> data = getData();
-        FileSystemFolder fs = new FileSystemFolder();
-        FileSystemFolder currentFolder = fs;
+        List<String> currentFolder = new ArrayList<>();
+        Map<String, Long> folderSizes = new HashMap<>();
+        folderSizes.put("/", 0L);
+
         for (int i=0; i<data.size(); i++) {
             String[] line = data.get(i).split(" ");
             if ("cd".equals(line[1])) {
                 if ("/".equals(line[2])) {
-                    currentFolder = currentFolder.goToRoot();
+                    currentFolder.clear();
                 } else if ("..".equals(line[2])) {
-                    currentFolder = currentFolder.goToParent();
+                    currentFolder.remove(currentFolder.size()-1);
                 } else {
-                    currentFolder = currentFolder.goToFolder(line[2]);
+                    currentFolder.add(line[2]);
                 }
             } else if ("ls".equals(line[1])) {
+                AtomicLong folderSize = new AtomicLong();
                 for (int j=i+1; j<data.size(); j++) {
                     i++;
                     if (data.get(j).charAt(0) == '$') {
@@ -58,11 +62,16 @@ public class Day07 extends Day {
                     }
                     String[] file = data.get(j).split(" ");
                     if (!"dir".equals(file[0])) {
-                        currentFolder.addFile(file[1], Long.parseLong(file[0], 10));
+                        folderSize.addAndGet(Long.parseLong(file[0], 10));
                     }
+                }
+                for (int j=currentFolder.size(); j>=0; j--) {
+                    String folder = "/" + String.join("/", currentFolder.subList(0,j));
+                    folderSizes.putIfAbsent(folder, 0L);
+                    folderSizes.compute(folder, (k,v)->v +=  folderSize.longValue());
                 }
             }
         }
-        return fs;
+        return folderSizes;
     }
 }
