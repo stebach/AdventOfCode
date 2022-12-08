689cf86a87f1a3558b5397cd86f2218bced1c4f7
ste <ste@ste.li>
2022-12-08 07:24:03 +0100

    Day 8

--- src/main/java/li/ste/adventofcode/year2022/Day08.java
+++ src/main/java/li/ste/adventofcode/year2022/Day08.java
@@ -3,6 +3,9 @@ package li.ste.adventofcode.year2022;
 import li.ste.adventofcode.utils.Day;
 import li.ste.adventofcode.utils.InputProvider;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Day08 extends Day {
     public static void main(String[] args) {
         Day day = new Day08(new InputProvider());
@@ -15,7 +18,63 @@ public class Day08 extends Day {
 
     @Override
     public void run() {
-        setSolution1("@todo");
-        setSolution2("@todo");
+        List<List<Integer>> trees = new ArrayList<>();
+        List<String> data = getData();
+        for (String line : data) {
+            List<Integer> treeRow = new ArrayList<>();
+            for (char tree : line.toCharArray()) {
+                treeRow.add(tree-'0');
+            }
+            trees.add(treeRow);
+        }
+
+        int visibleFromOutside = 0;
+        int bestScenicScore = 0;
+        for (int y=0; y<trees.size(); y++) {
+            for (int x=0; x<trees.get(y).size(); x++) {
+                if (
+                        checkOnlySmallerTrees(x,y,0,-1,trees) // north
+                        || checkOnlySmallerTrees(x,y,1,0,trees) // east
+                        || checkOnlySmallerTrees(x,y,0,1,trees) // south
+                        || checkOnlySmallerTrees(x,y,-1,0,trees) // west
+                ) {
+                    visibleFromOutside++;
+                }
+
+                int scenicScore = checkVisibleTrees(x,y,0,-1,trees)
+                        * checkVisibleTrees(x,y,1,0,trees)
+                        * checkVisibleTrees(x,y,0,1,trees)
+                        * checkVisibleTrees(x,y,-1,0,trees);
+                if (scenicScore > bestScenicScore) {
+                    bestScenicScore = scenicScore;
+                }
+
+            }
+        }
+
+        setSolution1(visibleFromOutside);
+        setSolution2(bestScenicScore);
+    }
+
+    private int checkVisibleTrees(int startX, int startY, int xChange, int yChange, List<List<Integer>> trees) {
+        int mySize = trees.get(startY).get(startX);
+        int visible = 0;
+        for (int y=startY+yChange, x=startX+xChange; y>=0 && y<trees.size() && x>=0 && x<trees.get(y).size(); y += yChange, x += xChange) {
+            visible ++;
+            if (trees.get(y).get(x) >= mySize) {
+                break;
+            }
+        }
+        return visible;
+    }
+
+    private boolean checkOnlySmallerTrees(int startX, int startY, int xChange, int yChange, List<List<Integer>> trees) {
+        int mySize = trees.get(startY).get(startX);
+        for (int y=startY+yChange, x=startX+xChange; y>=0 && y<trees.size() && x>=0 && x<trees.get(y).size(); y += yChange, x += xChange) {
+            if (trees.get(y).get(x) >= mySize) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/li/ste/adventofcode/year2022/TestDay08.java
+++ src/test/java/li/ste/adventofcode/year2022/TestDay08.java
@@ -20,11 +20,11 @@ public class TestDay08 {
 
     @Test
     void testSolution1() {
-        assertEquals("@todo", day.getSolution1()); // TODO: check solution 1 of day 08
+        assertEquals("21", day.getSolution1());
     }
 
     @Test
     void testSolution2() {
-        assertEquals("@todo", day.getSolution2()); // TODO: check solution 2 of day 08
+        assertEquals("8", day.getSolution2());
     }
 }
