7b5dc1576beb43028f50fec0d3dfd3ae73641ce6
ste <ste@ste.li>
2022-12-10 07:27:20 +0100

    Day 10

--- src/main/java/li/ste/adventofcode/year2022/Day10.java
+++ src/main/java/li/ste/adventofcode/year2022/Day10.java
@@ -3,6 +3,9 @@ package li.ste.adventofcode.year2022;
 import li.ste.adventofcode.utils.Day;
 import li.ste.adventofcode.utils.InputProvider;
 
+import java.util.*;
+import java.util.HashMap;
+
 public class Day10 extends Day {
     public static void main(String[] args) {
         Day day = new Day10(new InputProvider());
@@ -15,7 +18,49 @@ public class Day10 extends Day {
 
     @Override
     public void run() {
-        setSolution1("@todo");
-        setSolution2("@todo");
+        Scanner scanner = getScanner();
+        int x = 1;
+        int instructionDuration = 1;
+        int signalStrength = 0;
+        List<Integer> cyclesToCheck = Arrays.asList(20, 60, 100, 140, 180, 220);
+        String instruction="";
+        StringBuilder display = new StringBuilder();
+
+        Map<String, Integer> instructionDurations = new HashMap<>();
+        instructionDurations.put("noop",1);
+        instructionDurations.put("addx",2);
+
+        for (int cycle = 1; cycle <= 240; cycle++) {
+            //start of cycle
+            instructionDuration--;
+            if (instructionDuration == 0) { // get next instruction
+                instruction = scanner.next();
+                instructionDuration = instructionDurations.get(instruction);
+            }
+
+            //mid of cycle
+            if (cyclesToCheck.contains(cycle)) {
+                signalStrength += cycle * x;
+            }
+
+            if ((cycle -1) % 40 <= x+1 && (cycle -1) % 40 >= x-1) {
+                display.append("#");
+            } else {
+                display.append(".");
+            }
+
+            if (cycle % 40 == 0) {
+                display.append("\n");
+            }
+
+            //end of cycle
+            if ("addx".equals(instruction) && instructionDuration == 1) {
+                int toAdd = scanner.nextInt();
+                x += toAdd;
+            }
+        }
+
+        setSolution1(signalStrength);
+        setSolution2("\n" + display);
     }
 }
