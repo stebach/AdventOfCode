aa853dd4bd1a3581c3767db1ee4d31e943700030
ste <ste@ste.li>
2022-12-17 10:11:58 +0100

    Day 17

--- src/test/java/li/ste/adventofcode/year2022/TestDay17.java
+++ src/test/java/li/ste/adventofcode/year2022/TestDay17.java
@@ -20,11 +20,11 @@ public class TestDay17 {
 
     @Test
     void testSolution1() {
-        assertEquals("@todo", day.getSolution1()); // TODO: check solution 1 of day 17
+        assertEquals("3068", day.getSolution1());
     }
 
     @Test
     void testSolution2() {
-        assertEquals("@todo", day.getSolution2()); // TODO: check solution 2 of day 17
+        assertEquals("1514285714288", day.getSolution2());
     }
 }
