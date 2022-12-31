5e7f202562f18044f9851635926c65104f58d6aa
ste <ste@ste.li>
2022-12-31 09:44:43 +0100

    2020 day 13

--- src/main/java/li/ste/adventofcode/year2020/Day13.java
+++ src/main/java/li/ste/adventofcode/year2020/Day13.java
@@ -2,6 +2,7 @@ package li.ste.adventofcode.year2020;
 
 import li.ste.adventofcode.utils.Day;
 import li.ste.adventofcode.utils.InputProvider;
+import java.util.*;
 
 public class Day13 extends Day {
     public static void main(String[] args) {
@@ -15,7 +16,47 @@ public class Day13 extends Day {
 
     @Override
     public void run() {
-        setSolution1("@todo");
-        setSolution2("@todo");
+        Scanner scanner = getScanner();
+        int currentTime = Integer.parseInt(scanner.nextLine());
+        String[] buslinesStringArray = scanner.nextLine().split(",");
+        int[] buslines = Arrays.stream(buslinesStringArray).filter(r->!r.equals("x")).mapToInt(Integer::parseInt).toArray();
+
+        int shortestTime = Integer.MAX_VALUE;
+        int shortestTimeBusLine = 0;
+        for (int busline : buslines) {
+            int timeToWait = busline - (currentTime % busline);
+            if (timeToWait < shortestTime) {
+                shortestTime = timeToWait;
+                shortestTimeBusLine = busline;
+            }
+        }
+
+        setSolution1(shortestTimeBusLine * shortestTime);
+
+        buslines = Arrays.stream(buslinesStringArray).mapToInt(r -> r.equals("x") ? 1 : Integer.parseInt(r)).toArray();
+
+        List<long[]> buslinesWithPosition = new ArrayList<>();
+        long product = 1;
+        for (int i = 0; i < buslines.length; i += 1) {
+            if (buslines[i] != 1) {
+                buslinesWithPosition.add(new long[] { buslines[i], i % buslines[i], 0, 0 });
+                product *= buslines[i];
+            }
+        }
+
+        // Chinese Remainder Theorem
+        for (long[] longs : buslinesWithPosition) {
+            long number = (product / longs[0]);
+
+            int multiplicator = 0;
+            while (((number * multiplicator) % longs[0]) != longs[1]) {
+                multiplicator += 1;
+            }
+            longs[2] = number * multiplicator;
+        }
+
+        long sum = buslinesWithPosition.stream().mapToLong(r->r[2]).sum();
+
+        setSolution2(product % (sum % product));
     }
 }
--- src/test/java/li/ste/adventofcode/year2020/TestDay13.java
+++ src/test/java/li/ste/adventofcode/year2020/TestDay13.java
@@ -20,11 +20,11 @@ public class TestDay13 {
 
     @Test
     void testSolution1() {
-        assertEquals("@todo", day.getSolution1()); // TODO: check solution 1 of day 13
+        assertEquals("295", day.getSolution1());
     }
 
     @Test
     void testSolution2() {
-        assertEquals("@todo", day.getSolution2()); // TODO: check solution 2 of day 13
+        assertEquals("1068781", day.getSolution2());
     }
 }
