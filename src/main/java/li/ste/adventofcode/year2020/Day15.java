46f1d0e110c63c8f9b0a9accf660d81c5996bc1a
ste <ste@ste.li>
2023-01-03 07:33:33 +0100

    2020 day 15

--- src/main/java/li/ste/adventofcode/year2020/Day15.java
+++ src/main/java/li/ste/adventofcode/year2020/Day15.java
@@ -3,6 +3,11 @@ package li.ste.adventofcode.year2020;
 import li.ste.adventofcode.utils.Day;
 import li.ste.adventofcode.utils.InputProvider;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Day15 extends Day {
     public static void main(String[] args) {
         Day day = new Day15(new InputProvider());
@@ -15,7 +20,38 @@ public class Day15 extends Day {
 
     @Override
     public void run() {
-        setSolution1("@todo");
-        setSolution2("@todo");
+        Scanner scanner = getScanner();
+        int[] startNumbers = Arrays.stream(scanner.nextLine().split(",")).mapToInt(Integer::parseInt).toArray();
+        int round = 1;
+
+        Map<Integer, Integer> lastOccurence = new HashMap<>();
+
+        while (round <= startNumbers.length) {
+            lastOccurence.put(startNumbers[round-1], round);
+            round++;
+        }
+
+        int solution1 = 0;
+
+        int nextNumber = 0;
+        int currentNumber = 0;
+        while (round < 30000001) {
+            currentNumber = nextNumber;
+            if (lastOccurence.containsKey(nextNumber)) {
+                nextNumber = round - lastOccurence.get(nextNumber);
+            } else {
+                nextNumber = 0;
+            }
+            lastOccurence.put(currentNumber, round);
+
+            if (round == 2020) {
+                solution1 = currentNumber;
+            }
+
+            round++;
+        }
+
+        setSolution1(solution1);
+        setSolution2(currentNumber);
     }
 }
--- src/test/java/li/ste/adventofcode/year2020/TestDay15.java
+++ src/test/java/li/ste/adventofcode/year2020/TestDay15.java
@@ -20,11 +20,11 @@ public class TestDay15 {
 
     @Test
     void testSolution1() {
-        assertEquals("@todo", day.getSolution1()); // TODO: check solution 1 of day 15
+        assertEquals("1836", day.getSolution1());
     }
 
     @Test
     void testSolution2() {
-        assertEquals("@todo", day.getSolution2()); // TODO: check solution 2 of day 15
+        assertEquals("362", day.getSolution2());
     }
 }
