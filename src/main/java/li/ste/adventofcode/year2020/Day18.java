e0c1445f6a916927e67f17967537808d6732ca89
ste <ste@ste.li>
2023-01-04 12:43:02 +0100

    2020 day 18

--- src/main/java/li/ste/adventofcode/year2020/Day18.java
+++ src/main/java/li/ste/adventofcode/year2020/Day18.java
@@ -1,9 +1,20 @@
 package li.ste.adventofcode.year2020;
 
+import li.ste.adventofcode.utils.AdventOfCodeException;
 import li.ste.adventofcode.utils.Day;
 import li.ste.adventofcode.utils.InputProvider;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Day18 extends Day {
+
+    static Pattern patternParentheses = Pattern.compile("\\(([^()]+)\\)");
+    static Pattern patternAddition = Pattern.compile("([0-9]+) \\+ ([0-9]+)");
     public static void main(String[] args) {
         Day day = new Day18(new InputProvider());
         day.solvePuzzles();
@@ -15,7 +26,47 @@ public class Day18 extends Day {
 
     @Override
     public void run() {
-        setSolution1("@todo");
-        setSolution2("@todo");
+        Scanner scanner = getScanner();
+        List<String> calculations = new ArrayList<>();
+        while (scanner.hasNextLine()) {
+            calculations.add(scanner.nextLine());
+        }
+
+        setSolution1(calculations.stream().mapToLong(this::calc).sum());
+        setSolution2(calculations.stream().mapToLong(this::advancedCalc).sum());
+    }
+
+    public long calc(String calculation) {
+        Matcher matcher = patternParentheses.matcher(calculation);
+        while (matcher.find()) {
+            calculation = calculation.replace(matcher.group(0), "" + calc(matcher.group(1)));
+            matcher = patternParentheses.matcher(calculation);
+        }
+        List<String> parts = new ArrayList<>(Arrays.stream(calculation.split(" ")).toList());
+        long retVal = Long.parseLong(parts.remove(0));
+        while (!parts.isEmpty()) {
+            String operator = parts.remove(0);
+            switch (operator.charAt(0)) {
+                case '*' -> retVal *= Long.parseLong(parts.remove(0));
+                case '+' -> retVal += Long.parseLong(parts.remove(0));
+                default -> throw new AdventOfCodeException("UNKNOWN OPERATOR: " + operator);
+            }
+        }
+        return retVal;
+    }
+
+    public long advancedCalc(String calculation) {
+        Matcher matcher = patternParentheses.matcher(calculation);
+        while (matcher.find()) {
+            calculation = calculation.replace(matcher.group(0), "" + advancedCalc(matcher.group(1)));
+            matcher = patternParentheses.matcher(calculation);
+        }
+        matcher = patternAddition.matcher(calculation);
+        while (matcher.find()) {
+            calculation = calculation.substring(0,matcher.start(0)) + calc(matcher.group(0)) + calculation.substring(matcher.end(0));
+            matcher = patternAddition.matcher(calculation);
+        }
+
+        return calc(calculation);
     }
 }
--- src/test/java/li/ste/adventofcode/year2020/TestDay18.java
+++ src/test/java/li/ste/adventofcode/year2020/TestDay18.java
@@ -19,12 +19,31 @@ public class TestDay18 {
     }
 
     @Test
+    void testCalc() {
+        assertEquals(26, day.calc("2 * 3 + (4 * 5)"));
+        assertEquals(437, day.calc("5 + (8 * 3 + 9 + 3 * 4 * 3)"));
+        assertEquals(12240, day.calc("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"));
+        assertEquals(13632, day.calc("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"));
+    }
+
+    @Test
+    void testAdvancedCalc() {
+        assertEquals(46, day.advancedCalc("2 * 3 + (4 * 5)"));
+        assertEquals(1445, day.advancedCalc("5 + (8 * 3 + 9 + 3 * 4 * 3)"));
+        assertEquals(669060, day.advancedCalc("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))"));
+        assertEquals(23340, day.advancedCalc("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"));
+        assertEquals(6475593600L, day.advancedCalc("((5 * 6 + 7 + 4 + 3) * 9) * (4 + 8 * 9 + (5 + 2 + 2 + 4 * 7) + (9 * 7 + 4 * 5 + 3) * 2) + 6 * 6 * (7 * 2 + 5) + 7"));
+        assertEquals(319L, day.advancedCalc("8 + 3 + 8 + 300"));
+        assertEquals(1435392L, day.advancedCalc("(8 + 3 + 8 + (3 * 4 * 3 + 4 + 9 + 9)) + (5 + 3 + (2 + 5) + 6 + (8 + 8)) * 6 * 8 * (4 + 8) * 7"));
+    }
+
+    @Test
     void testSolution1() {
-        assertEquals("@todo", day.getSolution1()); // TODO: check solution 1 of day 18
+        assertEquals("26335", day.getSolution1());
     }
 
     @Test
     void testSolution2() {
-        assertEquals("@todo", day.getSolution2()); // TODO: check solution 2 of day 18
+        assertEquals("693891", day.getSolution2());
     }
 }
