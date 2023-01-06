e4ec4eab8cc16dabc1d7497674140b591b481a20
ste <ste@ste.li>
2023-01-06 23:12:38 +0100

    2020 day 23

--- src/main/java/li/ste/adventofcode/year2020/Day23.java
+++ src/main/java/li/ste/adventofcode/year2020/Day23.java
@@ -3,6 +3,10 @@ package li.ste.adventofcode.year2020;
 import li.ste.adventofcode.utils.Day;
 import li.ste.adventofcode.utils.InputProvider;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Day23 extends Day {
     public static void main(String[] args) {
         Day day = new Day23(new InputProvider());
@@ -15,7 +19,87 @@ public class Day23 extends Day {
 
     @Override
     public void run() {
-        setSolution1("@todo");
-        setSolution2("@todo");
+        Scanner scanner = getScanner();
+        String line = scanner.nextLine();
+
+
+        List<Integer> cups = line.chars().mapToObj(r->Integer.valueOf(r-'0')).toList();
+        int[] nextCup = new int[cups.size() + 1];
+        for (int i = 0; i < cups.size(); i++) {
+            nextCup[cups.get(i)] = i + 1 == cups.size() ? 0 : i + 1;
+        }
+
+        moveCups(cups, nextCup, 100);
+        setSolution1(formatResult(cups, nextCup));
+
+        cups = new ArrayList<>(line.chars().mapToObj(r->Integer.valueOf(r-'0')).toList());
+        for (int i = 10; i <= 1_000_000 ; i++) {
+            cups.add(i);
+        }
+        nextCup = new int[cups.size() + 1];
+        for (int i = 0; i < cups.size(); i++) {
+            nextCup[cups.get(i)] = i + 1 == cups.size() ? 0 : i + 1;
+        }
+
+        moveCups(cups, nextCup, 10_000_000);
+
+        int num1 = cups.get(nextCup[1]);
+        int num2 = cups.get(nextCup[num1]);
+
+        long solution2 = (long)num1 * num2;
+
+        setSolution2(solution2);
+    }
+
+    public void moveCups(List<Integer> cups, int[] nextCup, int moves) {
+        int idx = 0;
+        for (int i = 0; i < moves; i++) {
+            int number = cups.get(idx);
+            int[] move = new int[3];
+            for (int j = 0; j < 3; j++) {
+                move[j] = nextCup[number];
+                number = cups.get(move[j]);
+            }
+            // remove 3
+            nextCup[cups.get(idx)] = nextCup[number];
+
+            int destinationNumber = cups.get(idx) - 1;
+            while (destinationNumber < 1 || destinationNumber > cups.size() || cups.get(move[0]) == destinationNumber || cups.get(move[1]) == destinationNumber || cups.get(move[2]) == destinationNumber) {
+                destinationNumber -= 1;
+
+                if (destinationNumber < 1) {
+                    destinationNumber = cups.size();
+                }
+            }
+
+            nextCup[cups.get(move[2])] = nextCup[destinationNumber];
+            nextCup[destinationNumber] = move[0];
+
+            idx = nextCup[cups.get(idx)];
+        }
+    }
+
+    public String formatResult(List<Integer> cups, int[] nextCup) {
+        int start = nextCup[1];
+        StringBuilder sb = new StringBuilder();
+        sb.append(cups.get(start));
+        int next = nextCup[cups.get(start)];
+        while (cups.get(next) != 1) {
+            sb.append(cups.get(next));
+            next = nextCup[cups.get(next)];
+        }
+        return sb.toString();
+    }
+
+    public String cupsToString(List<Integer> cups, int[] nextCupClone) {
+        StringBuilder sb = new StringBuilder();
+        int first = 0;
+        sb.append(cups.get(first));
+        int next = nextCupClone[cups.get(first)];
+        while (next != first) {
+            sb.append(cups.get(next));
+            next = nextCupClone[cups.get(next)];
+        }
+        return sb.toString();
     }
 }
--- src/test/java/li/ste/adventofcode/year2020/TestDay23.java
+++ src/test/java/li/ste/adventofcode/year2020/TestDay23.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DisplayNameGeneration(li.ste.adventofcode.DisplayNameGenerator.class)
@@ -19,12 +22,94 @@ public class TestDay23 {
     }
 
     @Test
+    void testMoves() {
+        List<Integer> cups = Arrays.asList(3,8,9,1,2,5,4,6,7);
+        int[] nextCup = new int[cups.size() + 1];
+        for (int i = 0; i < cups.size(); i++) {
+            nextCup[cups.get(i)] = i + 1 == cups.size() ? 0 : i + 1;
+        }
+        assertEquals("389125467", day.cupsToString(cups, nextCup));
+
+        int[] nextCupClone = nextCup.clone();
+        day.moveCups(cups, nextCupClone, 1);
+        assertEquals("328915467", day.cupsToString(cups, nextCupClone));
+        nextCupClone = nextCup.clone();
+        day.moveCups(cups, nextCupClone, 2);
+        assertEquals("325467891",  day.cupsToString(cups, nextCupClone));
+        nextCupClone = nextCup.clone();
+        day.moveCups(cups, nextCupClone, 3);
+        assertEquals("346725891",  day.cupsToString(cups, nextCupClone));
+        nextCupClone = nextCup.clone();
+        day.moveCups(cups, nextCupClone, 4);
+        assertEquals("325846791",  day.cupsToString(cups, nextCupClone));
+        nextCupClone = nextCup.clone();
+        day.moveCups(cups, nextCupClone, 5);
+        assertEquals("367925841",  day.cupsToString(cups, nextCupClone));
+        nextCupClone = nextCup.clone();
+        day.moveCups(cups, nextCupClone, 6);
+        assertEquals("367258419",  day.cupsToString(cups, nextCupClone));
+        nextCupClone = nextCup.clone();
+        day.moveCups(cups, nextCupClone, 7);
+        assertEquals("367419258",  day.cupsToString(cups, nextCupClone));
+        nextCupClone = nextCup.clone();
+        day.moveCups(cups, nextCupClone, 8);
+        assertEquals("392674158",  day.cupsToString(cups, nextCupClone));
+        nextCupClone = nextCup.clone();
+        day.moveCups(cups, nextCupClone, 9);
+        assertEquals("392657418",  day.cupsToString(cups, nextCupClone));
+        nextCupClone = nextCup.clone();
+        day.moveCups(cups, nextCupClone, 10);
+        assertEquals("374192658",  day.cupsToString(cups, nextCupClone));
+    }
+
+
+    @Test
+    void testFormatResult() {
+        List<Integer> cups = Arrays.asList(3,8,9,1,2,5,4,6,7);
+        int[] nextCup = new int[cups.size() + 1];
+        for (int i = 0; i < cups.size(); i++) {
+            nextCup[cups.get(i)] = i + 1 == cups.size() ? 0 : i + 1;
+        }
+
+        int[] nextCupClone = nextCup.clone();
+        day.moveCups(cups, nextCupClone, 1);
+        assertEquals("54673289", day.formatResult(cups, nextCupClone));
+        nextCupClone = nextCup.clone();
+        day.moveCups(cups, nextCupClone, 2);
+        assertEquals("32546789", day.formatResult(cups, nextCupClone));
+        nextCupClone = nextCup.clone();
+        day.moveCups(cups, nextCupClone, 3);
+        assertEquals("34672589", day.formatResult(cups, nextCupClone));
+        nextCupClone = nextCup.clone();
+        day.moveCups(cups, nextCupClone, 4);
+        assertEquals("32584679", day.formatResult(cups, nextCupClone));
+        nextCupClone = nextCup.clone();
+        day.moveCups(cups, nextCupClone, 5);
+        assertEquals("36792584", day.formatResult(cups, nextCupClone));
+        nextCupClone = nextCup.clone();
+        day.moveCups(cups, nextCupClone, 6);
+        assertEquals("93672584", day.formatResult(cups, nextCupClone));
+        nextCupClone = nextCup.clone();
+        day.moveCups(cups, nextCupClone, 7);
+        assertEquals("92583674", day.formatResult(cups, nextCupClone));
+        nextCupClone = nextCup.clone();
+        day.moveCups(cups, nextCupClone, 8);
+        assertEquals("58392674", day.formatResult(cups, nextCupClone));
+        nextCupClone = nextCup.clone();
+        day.moveCups(cups, nextCupClone, 9);
+        assertEquals("83926574", day.formatResult(cups, nextCupClone));
+        nextCupClone = nextCup.clone();
+        day.moveCups(cups, nextCupClone, 10);
+        assertEquals("92658374", day.formatResult(cups, nextCupClone));
+    }
+
+    @Test
     void testSolution1() {
-        assertEquals("@todo", day.getSolution1()); // TODO: check solution 1 of day 23
+        assertEquals("67384529", day.getSolution1());
     }
 
     @Test
     void testSolution2() {
-        assertEquals("@todo", day.getSolution2()); // TODO: check solution 2 of day 23
+        assertEquals("149245887792", day.getSolution2());
     }
 }
