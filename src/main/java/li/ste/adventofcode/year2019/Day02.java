78619ba2fa59cea98f31f701330f520115b6b1b0
ste <ste@ste.li>
2023-01-09 21:43:13 +0100

    2019 day 2

--- src/main/java/li/ste/adventofcode/year2019/Day02.java
+++ src/main/java/li/ste/adventofcode/year2019/Day02.java
@@ -1,5 +1,6 @@
 package li.ste.adventofcode.year2019;
 
+import li.ste.adventofcode.utils.AdventOfCodeException;
 import li.ste.adventofcode.utils.Day;
 import li.ste.adventofcode.utils.InputProvider;
 
@@ -18,11 +19,50 @@ public class Day02 extends Day {
     @Override
     public void run() {
         Scanner scanner = getScanner();
-        while (scanner.hasNextLine()) {
-            System.out.println(scanner.nextLine());
+        int[] values = Arrays.stream(scanner.nextLine().split(",")).mapToInt(Integer::parseInt).toArray();
+
+        IntCode intCode = new IntCode();
+
+        int[] result = intCode.run(values, 12, 2);
+
+        setSolution1(result[0]);
+
+        int result2 = 0;
+        loop:
+        for (int noun = 1; noun < 100; noun += 1) {
+            for (int verb = 1; verb < 100; verb += 1) {
+                result = intCode.run(values, noun, verb);
+                if (result[0] == 19690720) {
+                    result2 = noun * 100 + verb;
+                    break loop;
+                }
+            }
         }
 
-        setSolution1("@todo");
-        setSolution2("@todo");
+        setSolution2(result2);
+    }
+
+    private static class IntCode {
+        public int[] run(int[] memory, int noun, int verb) {
+            int[] localMemory = memory.clone();
+            localMemory[1] = noun;
+            localMemory[2] = verb;
+            int instructionPointer = 0;
+            while (localMemory[instructionPointer] != 99) {
+                switch (localMemory[instructionPointer]) {
+                    case 1 -> {
+                        localMemory[localMemory[instructionPointer + 3]] = localMemory[localMemory[instructionPointer + 1]] + localMemory[localMemory[instructionPointer + 2]];
+                        instructionPointer += 4;
+                    }
+                    case 2 -> {
+                        localMemory[localMemory[instructionPointer + 3]] = localMemory[localMemory[instructionPointer + 1]] * localMemory[localMemory[instructionPointer + 2]];
+                        instructionPointer += 4;
+                    }
+                    default ->
+                            throw new AdventOfCodeException("invalid operation: " + localMemory[instructionPointer] + " at " + instructionPointer);
+                }
+            }
+            return localMemory;
+        }
     }
 }
