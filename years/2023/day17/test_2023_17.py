f2be6975e568cdffe803df5b6a4ffe242c69e9bc
Stefan Bächler <stefan.baechler@nexum.com>
2023-12-18 07:22:24 +0100

    2023 day 17 cleanup

--- years/2023/day17/test_2023_17.py
+++ years/2023/day17/test_2023_17.py
@@ -22,30 +22,6 @@ def puzzle_input():
 def test_part1_example1(puzzle_input):
     assert subject.part1(puzzle_input) == 102
 
-@pytest.mark.skip(reason="not yet implemented")
-def test_part1_example2(puzzle_input):
-    assert subject.part1(puzzle_input) == 1
-
-@pytest.mark.skip(reason="not yet implemented")
-def test_part1_example3(puzzle_input):
-    assert subject.part1(puzzle_input) == 1
-
-@pytest.mark.skip(reason="not yet implemented")
-def test_part1_example4(puzzle_input):
-    assert subject.part1(puzzle_input) == 1
-
 def test_part2_example1(puzzle_input):
-    assert subject.part2(puzzle_input) == 1
-
-@pytest.mark.skip(reason="not yet implemented")
-def test_part2_example2(puzzle_input):
-    assert subject.part2(puzzle_input) == 1
-
-@pytest.mark.skip(reason="not yet implemented")
-def test_part2_example3(puzzle_input):
-    assert subject.part2(puzzle_input) == 1
-
-@pytest.mark.skip(reason="not yet implemented")
-def test_part2_example4(puzzle_input):
-    assert subject.part2(puzzle_input) == 1
+    assert subject.part2(puzzle_input) == 94
 
