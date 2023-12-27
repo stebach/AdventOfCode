5ab58dcfb941a19bc70da266c40460f08f80ce27
Stefan Bächler <stefan.baechler@nexum.com>
2023-12-27 14:48:58 +0100

    2018 day 18 cleanup

--- years/2018/day18/test_2018_18.py
+++ years/2018/day18/test_2018_18.py
@@ -179,31 +179,3 @@ def test_part1_example1(puzzle_input):
     data = subject.parse_lines(puzzle_input)
     assert subject.part1(data, 10) == 1147
 
-
-@pytest.mark.skip(reason="not yet implemented")
-def test_part1_example2(puzzle_input):
-    assert subject.part1(puzzle_input) == 1
-
-@pytest.mark.skip(reason="not yet implemented")
-def test_part1_example3(puzzle_input):
-    assert subject.part1(puzzle_input) == 1
-
-@pytest.mark.skip(reason="not yet implemented")
-def test_part1_example4(puzzle_input):
-    assert subject.part1(puzzle_input) == 1
-
-def test_part2_example1(puzzle_input):
-    assert subject.part2(puzzle_input) == 1
-
-@pytest.mark.skip(reason="not yet implemented")
-def test_part2_example2(puzzle_input):
-    assert subject.part2(puzzle_input) == 1
-
-@pytest.mark.skip(reason="not yet implemented")
-def test_part2_example3(puzzle_input):
-    assert subject.part2(puzzle_input) == 1
-
-@pytest.mark.skip(reason="not yet implemented")
-def test_part2_example4(puzzle_input):
-    assert subject.part2(puzzle_input) == 1
-
