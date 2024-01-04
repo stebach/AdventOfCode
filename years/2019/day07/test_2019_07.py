c10f56a3c7871c73c1e55538ea0447b2e9b1e55e
Stefan Bächler <stefan.baechler@nexum.com>
2024-01-04 23:26:00 +0100

    fix for tests 2019 day 7

--- years/2019/day07/test_2019_07.py
+++ years/2019/day07/test_2019_07.py
@@ -3,12 +3,24 @@ import pytest
 
 
 def test_intcode():
-    assert subject.intcode([1,9,10,3,2,3,11,0,99,30,40,50]) == [3500,9,10,70,2,3,11,0,99,30,40,50]
-    assert subject.intcode([1,0,0,0,99]) == [2,0,0,0,99]
-    assert subject.intcode([2,3,0,3,99]) == [2,3,0,6,99]
-    assert subject.intcode([2,4,4,5,99,0]) == [2,4,4,5,99,9801]
-    assert subject.intcode([1,1,1,4,99,5,6,0,99]) == [30,1,1,4,2,5,6,0,99]
-    assert subject.intcode([1002,4,3,4,33]) == [1002,4,3,4,99]
+    data = [1,9,10,3,2,3,11,0,99,30,40,50]
+    subject.intcode(data)
+    assert data == [3500,9,10,70,2,3,11,0,99,30,40,50]
+    data = [1,0,0,0,99]
+    subject.intcode(data)
+    assert data == [2,0,0,0,99]
+    data = [2,3,0,3,99]
+    subject.intcode(data)
+    assert data == [2,3,0,6,99]
+    data = [2,4,4,5,99,0]
+    subject.intcode(data)
+    assert data == [2,4,4,5,99,9801]
+    data = [1,1,1,4,99,5,6,0,99]
+    subject.intcode(data)
+    assert data == [30,1,1,4,2,5,6,0,99]
+    data = [1002,4,3,4,33]
+    subject.intcode(data)
+    assert data == [1002,4,3,4,99]
     output = []
     for x in range(7,10):
         input = [x]
