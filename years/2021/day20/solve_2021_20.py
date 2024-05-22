44f99edec65ad791f4c28dfa88ffa12e750316ee
Stefan Bächler <stefan.baechler@nexum.com>
2024-05-22 17:48:28 +0200

    cleanup

--- years/2021/day20/solve_2021_20.py
+++ years/2021/day20/solve_2021_20.py
@@ -84,39 +84,3 @@ if __name__ == "__main__":
     solutions = solve(puzzle_input)
     print("\n".join(str(solution) for solution in solutions))
 
-def tools():
-    print('map example:')
-    #import networkx as nx
-    #import matplotlib.pyplot as plt
-    #graph = nx.Graph()
-    #graph.add_edge((0,0),(0,1))
-    #graph.add_edge((0,1),(1,1))
-    #graph.add_edge((1,0),(1,1))
-    #nx.shortest_path(graph, (0,0), (1,0))
-    #nx.draw(graph, pos={x:x for x in graph.nodes}, with_labels=True)
-    #graph.add_edge(1, 2, weight=3)
-    #graph.add_edge(1, 3, weight=7, capacity=15, length=342.7)
-    #nx.dijkstra_path(graph, (0,0), (1,0))
-    #nx.dijkstra_path(graph, (0,0), (1, 0), calc_path_weight)
-    #def calc_path_weight(from_node, to_node, edge_attributes):
-    #   return xxx
-    #plt.show()
-
-    print('numpy:')
-    #import numpy as np
-    #np.lcm(1,2)
-    #np.lcm.reduce([1,2,3,4])
-
-    print('z3:')
-    #import z3
-    #opt = z3.Solver()
-    #a = z3.Int('a')
-    #b = z3.Int('b')
-    #c = z3.Int('c')
-    #opt.add(a < 6, a > 0, b < 6, b > 0, c < 6, c > 0)
-    #if opt.check() == z3.unsat:
-    #    model = opt.model()
-    #    print(model)
-    #    print(model[a])
-    #    print(model[b])
-    #    print(model[c])
